60c5ac0e0a309872e6bb581011e2cee0da46e3c4
Roshu2 <kyumin1020@gmail.com>
2022-06-03 09:21:54 +0900

    1. static파일 생성 2.settings.py static연결

--- TenTaku/settings.py
+++ TenTaku/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,6 +83,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+#Static files
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 
 # Password validation

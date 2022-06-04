2416238377e50f7f1e444f4555fd24b6523a6101
원송희 <songheew1020@gmail.com>
2022-06-04 17:50:57 +0900

    장르 선택창 오류나서 다시 추가

--- TenTaku/views.py
+++ TenTaku/views.py
@@ -8,7 +8,7 @@ def detail_view(request):
 
 
 def select_genre(request):
-    return render(request, 'animation/genrepage.html')
+    return render(request, 'animation/select_genre.html')
 
 def genre_view(request):
     return render(request, 'animation/bookmark.html')

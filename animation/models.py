87e566713fdc8b9dd44c85adfb41bf6cc23c83ff
KimmyJay <fkim0801@gmail.com>
2022-06-04 23:00:28 +0900

    모델 목업

--- animation/models.py
+++ animation/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Genre(models.Model):
+    class Meta:
+        db_table = "genre"
+    name = models.CharField(max_length=70, default='')
+
+class Animation(models.Model):
+    class Meta:
+        db_table = "animation"
+    name = models.CharField(max_length=70, default='')
+    thumbnail = models.TextField(max_length=256, default='')
+    publisher = models.CharField(max_length=70, default='')
+    summary = models.TextField(max_length=256, default='')
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+

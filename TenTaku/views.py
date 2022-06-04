2227bc731d6779d2ba9e349268c3718299881574
Roshu2 <kyumin1020@gmail.com>
2022-06-04 18:19:12 +0900

    1. detail.view 수정

--- TenTaku/views.py
+++ TenTaku/views.py
@@ -3,9 +3,6 @@ from django.shortcuts import render
 def main_view(request):
     return render(request, 'animation/mainpage.html')
 
-def detail_view(request):
-    return render(request, 'animation/detail.html')
-
 def select_genre(request):
     return render(request, 'user/select_genre.html')
 

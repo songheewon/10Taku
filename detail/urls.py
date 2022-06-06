271846a708bd2cd720afcf8a7dd6e357e24e9335
KimmyJay <fkim0801@gmail.com>
2022-06-06 14:01:40 +0900

    유저 세팅

--- detail/urls.py
+++ detail/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('detail/', views.show_detail_view, name='detail-page'),
     path('comment/<int:id>', views.comment, name='comment'),
     path('comment/delete/<int:id>', views.delete_comment, name='delete-comment'),
     path('bookmark/', views.comment, name='comment'),

e467b91eb2299820e41644532d700866e0228bd0
Roshu2 <kyumin1020@gmail.com>
2022-06-06 18:07:07 +0900

    1. animation모델 수정

--- animation/models.py
+++ animation/models.py
@@ -11,9 +11,16 @@ class Genre(models.Model):
 class Animation(models.Model):
     class Meta:
         db_table = "animation"
-    name = models.CharField(max_length=70, default='')
-    thumbnail = models.TextField(max_length=256, default='')
-    publisher = models.CharField(max_length=70, default='')
-    summary = models.TextField(max_length=256, default='')
+    title = models.CharField(max_length=70, default='')
+    original_title = models.CharField(max_length=70, default='')
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    publisher = models.CharField(max_length=70, default='')
+    rated = models.CharField(max_length=70, default='')
+    broadcasted_date = models.CharField(max_length=70, default='')
+    chapters = models.CharField(max_length=70, default='')
+    summary = models.TextField(max_length=256, default='')
+    img = models.TextField(max_length=256, default='')
+
+
+
 

9ba034c286ca60f60130e93478246ab2c134acf2
Roshu2 <kyumin1020@gmail.com>
2022-06-11 15:55:11 +0900

    merge conflict

--- user/views.py
+++ user/views.py
@@ -30,19 +30,13 @@ def sign_up_view(request):
 
             exist_user = get_user_model().objects.filter(username=username)
             if exist_user:
-<<<<<<< HEAD
 
-                return render(request, 'user/sign_up.html', {'error': '사용자가 존재합니다'}) #사용자가 존재하기 때문에 저장x 회원가입페이지 다시띄움
-            else:
-                UserModel.objects.create_user(username=username, password=password, email=email) #밑의 주석코드를 한줄로 줄임
 
-=======
-                print('사용자 존재!')
                 return render(request, 'user/sign_up.html', {'error': '사용자가 존재합니다'})
             else:
                 UserModel.objects.create_user(username=username, password=password, email=email)
-                print('회원가입 성공')
->>>>>>> 12422786b2149f02bc7be9effd46129b99562bb5
+
+
                 return redirect('/login')
 
 
@@ -52,19 +46,13 @@ def login_view(request):
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
 
-<<<<<<< HEAD
-        if me is not None: #위의 코드에서 사용자 정보를 다비교하고 오기때문에 me만 사용한다. me가 비어있지않다면
-            auth.login(request, me) #내정보를 넣어준다.로그인 작업을 해준다.
-
-            return redirect('/select_genre')  #select_genre로 redirect
-=======
         me = auth.authenticate(request, username=username, password=password)
 
         if me is not None:
             auth.login(request, me)
-            print('로그인성공')
+
             return redirect('/select_genre')
->>>>>>> 12422786b2149f02bc7be9effd46129b99562bb5
+
         else:
 
             return render(request, 'user/login.html', {'error': '유저이름 혹은 비밀번호를 확인 해 주세요'})
